957f1f50e580f70241116782d3ec4260726d3266
Qasim Mehmood <qasim.mehmood@careem.com>
2025-05-20 14:03:49 +0500

    update log entities

--- src/capabilities/get-logs-for-entity.ts
+++ src/capabilities/get-logs-for-entity.ts
@@ -4,7 +4,6 @@ import { executeDql } from "./execute-dql";
 
 
 export const getLogsForEntity = async (dtClient: _OAuthHttpClient, entityId: string) => {
-  const dql = `fetch logs | filter dt.source_entity == "${entityId}"`;
-
+   const dql=`fetch logs | filter matchesValue(service,"${entityId}") | fields timestamp, content, loglevel, careem_domain, city_id, cluster_id, domain, error, headers.http_host, host, lat, lng, meta, method, span_id, url, user_id`
   return executeDql(dtClient, dql);
 };
